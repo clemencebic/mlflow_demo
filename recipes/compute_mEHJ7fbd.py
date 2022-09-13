682eee932636a463478ce0ef34dfdaa003b79467
admin <admin>
2022-09-13 17:39:36 +0200

    Saved recipe: MLFLOW_DEMO.compute_mEHJ7fbd

---
+++ recipes/compute_mEHJ7fbd.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import dataikuapi
+import os
+
+from dataikuapi.dss.ml import DSSPredictionMLTaskSettings
+
+import pandas as pd
+import mlflow.catboost
+from catboost import CatBoostClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer
+from datetime import datetimeu
+
+# Read recipe inputs
+training_data = dataiku.Dataset("training_data")
+training_data_df = training_data.get_dataframe()
+
+cat_cols= ["job", "marital", "education", "default", "housing","loan", "month"]
+cont_cols= ["age", "balance", "day", "duration", "campaign"]
+target= ["y"]
+
+# Train a catboost model on training data
+cat_col_idx = [df.columns.get_loc(c) for c in cat_cols]
+X = df.drop(target, axis=1)
+y = LabelBinarizer().fit_transform(df[target[0]])
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, train_size=0.7, random_state=1337)
+model = CatBoostClassifier(iterations=100, learning_rate=0.05, depth=15, eval_metric="AUC")
+model.fit(X_train, y_train,
+          cat_features=cat_col_idx,
+          eval_set=(X_val, y_val))
+
+# Save the model to a managed folder
+catboost_models_folder = dataiku.Folder("catboost_models")
+catboost_models_folder_dir = catboost_models_folder.get_path()
+ts = datetime.now().strftime("%Y%m%d-%H%M%S")
+model_dir = "{}/catboost-uci-bank-{}".format(catboost_models_folder_dir,ts)
+
+mlflow.catboost.save_model(model, model_dir)
+print("Model saved at {} !".format(os.path.abspath(model_dir)))
